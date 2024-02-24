2c7a7ef0188b08bdbfbf6e0b678e4d661867b129
theadamsabra <theadamsabra@gmail.com>
2024-02-24 01:54:19 +0000

    continuing the skeleton

--- torchmultimodal/models/llava/model.py
+++ torchmultimodal/models/llava/model.py
@@ -7,15 +7,28 @@ class LLaVA(nn.Module):
                  image_encoder: nn.Module,
                  text_encoder: nn.Module,
                  image_projection: nn.Module,
+                 multimodal_encoder: nn.Module,
                  *args, **kwargs
     ) -> None:
         super().__init__(*args, **kwargs)
         self.image_encoder = image_encoder
         self.text_encoder = text_encoder
         self.image_projection = image_projection
+        self.multimodal_encoder = multimodal_encoder
 
     def forward(self, 
-                image:Optional[Tensor] = None,
+                image: Optional[Tensor] = None,
                 text: Optional[Tensor] = None
     ):
-        pass
+        image_embeddings = self._encode_and_project_image(image)
+        text_embeddings = self.text_encoder(text)
+        # Connect embeddings:
+        joint_representation = self.multimodal_encoder(image_embeddings, text_embeddings)
+        # Pass through language model:
+        # Return response:
+    
+    def _encode_and_project_image(self,
+                                  image: Tensor 
+    ) -> Tensor:
+        # Ideally:
+        return self.image_projection(self.image_encoder(image))
